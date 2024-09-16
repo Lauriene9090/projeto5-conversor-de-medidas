fdd06b64b60e9e0f7cbf0e66b744073f052bcb5b
Lauriene <laurienea840@gmail.com>
2024-09-16 20:21:48 -0300

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-function converterMedida(metros, opcao) {
-  switch (opcao) {
-      case 'mm':
-          return metros * 1000;
-      case 'cm':
-          return metros * 100;
-      case 'dm':
-          return metros * 10;
-      case 'dam':
-          return metros / 10;
-      case 'hm':
-          return metros / 100;
-      case 'km':
-          return metros / 1000;
-      default:
-          return 'Opção inválida';
-  }
-}
-
-const valorEmMetros = parseFloat('Digite um valor em metros:');
-
-const unidadeDesejada = 'Escolha a unidade de medida (mm, cm, dm, dam, hm ou km):';
-
-const resultado = converterMedida(valorEmMetros, unidadeDesejada);
-
-if (typeof resultado === 'number') {
-  console.log(`Resultado: ${resultado} ${unidadeDesejada}`);
-} else {
-  console.log(resultado);
+let mValue = prompt("digite o valor em metros")
+let chooseValue = prompt("escolha uma opção: 1-milimetros, 2-centimetros, 3-decimetros, 4-decametros, 5-hectometros, 6-uilômetros")
+switch(chooseValue){
+  case 'milimetro':
+    alert(mValue*1000)
+    break
+  case 'centimetro':
+    alert(mValue*100)
+    break
+  case 'decimetro':
+    alert(mValue*10)
+    break
+  case 'decametro':
+    alert(mValue/10)
+    break
+  case 'hectometro':
+    alert(mValue)
+    break
+  case 'uilômetro':
+    alert(mValue/1000)
+    break
+  default:
+    alert('opção invalida!')
 }
